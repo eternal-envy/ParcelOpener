d1786ef99f17d213e4f6f48a216f87bbb6396330
eternal-envy <67515713+eternal-envy@users.noreply.github.com>
2024-04-17 10:45:04 +0300

    Updated program to parse lock statuses.

--- ParcelOpener/Program.cs
+++ ParcelOpener/Program.cs
@@ -7,10 +7,10 @@ public class Program
 {
     protected static void Main(string[] args)
     {
+        Console.WriteLine("Open lock");
+        const int registerAddress = 1005;
         Console.WriteLine("Enter device address (COM1):");
         var deviceAddress = Console.ReadLine();
-        Console.WriteLine("Enter register address:");
-        var registerAddress = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter value to write to register:");
         int registerValue;
         do
@@ -30,6 +30,24 @@ public class Program
 
         modbusClient.WriteSingleRegister(registerAddress, registerValue);
 
-        Console.WriteLine("Value written");
+        Console.WriteLine("Value written\nCheck status");
+
+        GetLockStatuses(deviceAddress, registerAddress);
     }
+
+    private List<bool> GetLocksStatuses(int deviceAddress, int registerAddress)
+        {
+            var readResult = Read(SlaveId, OpenLockRegisterAddress, 1);
+            if (readResult == null)
+                return null;
+
+            var result = new List<bool>();
+            var mask = 1;
+            for (byte i = 0; i < 16; i++)
+            {
+                result.Add((readResult[0] & mask) == 0);
+                mask <<= 1;
+            }
+            return result;
+        }
 }
